c7333577b2dd6629ab94493bedbf53f5708a6025
Patrick Blakey <p.blakey@kainos.com>
2020-02-27 15:50:51 +0000

    Remove error formatter

--- src/logger.ts
+++ src/logger.ts
@@ -44,7 +44,6 @@ class Logger implements ILogger {
         winston.format.timestamp({
           format: 'YYYY-MM-DD HH:mm:ss',
         }),
-        winston.format.errors({ stack: true }),
         winston.format.splat(),
         winston.format.json(),
         winston.format.colorize(),
