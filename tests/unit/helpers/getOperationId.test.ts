40592f6c74c9e32ea8fadf1ef9f293825e532421
Tom Billington <t.billington@kainos.com>
2020-06-15 10:56:21 +0100

    Always return an operation id

--- tests/unit/helpers/getOperationId.test.ts
+++ tests/unit/helpers/getOperationId.test.ts
@@ -1,15 +1,21 @@
 // eslint-disable-next-line import/no-unresolved
 import { Context } from '@azure/functions';
-
+import Util from 'applicationinsights/out/Library/Util';
 import getOperationId from '../../../src/helpers/getOperationId';
 
+/*
+  If there isn't a traceparent passed into Traceparent, the function Util.w3cTraceId
+  is used to generate a new operation id
+*/
+Util.w3cTraceId = jest.fn().mockReturnValue('new-trace-id');
+
 describe('getOperationId', () => {
-  test('should return an empty string if there is no traceContext', () => {
-    expect(getOperationId({} as Context)).toEqual('');
+  test('should return an new operation id if there is no traceContext', () => {
+    expect(getOperationId({} as Context)).toEqual('new-trace-id');
   });
 
   test('should return an empty string if ther is no traceParent', () => {
-    expect(getOperationId({ traceContext: {} } as Context)).toEqual('');
+    expect(getOperationId({ traceContext: {} } as Context)).toEqual('new-trace-id');
   });
 
   test('should return a trace id if a traceParent exists', () => {
