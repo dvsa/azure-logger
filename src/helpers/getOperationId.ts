b50c0299e52fc4c997335ceb0e81a5e20b47c972
Tom Billington <Cavalier07@users.noreply.github.com>
2020-06-15 11:14:33 +0100

    FTT-3568 - Generate operation id if missing (#21)
    * Always return an operation id
    
    * 2.0.1
    
    * Fixed typo

--- src/helpers/getOperationId.ts
+++ src/helpers/getOperationId.ts
@@ -4,7 +4,7 @@ import Traceparent from 'applicationinsights/out/Library/Traceparent';
 
 function getOperationId(context: Context): string {
   if (!context.traceContext || !context.traceContext.traceparent) {
-    return '';
+    return new Traceparent(undefined).traceId;
   }
   return new Traceparent(context.traceContext.traceparent).traceId;
 }
