c0593db0cbf90d61bfb691a841b7a70be3a7d64a
Mohammed Foysal <mohammedfoysal1997@gmail.com>
2020-09-08 15:57:17 +0100

    FTT-4341: Export method for extracting operation id

--- src/telemetry.ts
+++ src/telemetry.ts
@@ -1,7 +1,9 @@
 import { startOperation, wrapWithCorrelationContext, defaultClient } from 'applicationinsights';
+import getOperationId from './helpers/getOperationId';
 
 export default {
   startOperation,
   wrapWithCorrelationContext,
   defaultClient,
+  getOperationId,
 };
