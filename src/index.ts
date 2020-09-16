93f9be439311d7ac2d369c5b51321bef96b05a0a
Patrick Blakey <p.blakey@kainos.com>
2020-09-16 12:13:06 +0100

    Expose getOperationId helper

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import Logger from './logger';
 import ILogger from './ILogger';
 import azureFunctions from './azureFunctions';
+import getOperationId from './helpers/getOperationId';
 
 export {
-  Logger, ILogger, azureFunctions,
+  Logger, ILogger, azureFunctions, getOperationId,
 };
