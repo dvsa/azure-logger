b0cef5dc1603ebde35f1d3b1798cb37a5759d157
Patrick Blakey <p.blakey@kainos.com>
2020-09-16 14:36:09 +0100

    Update index

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,6 @@
 import Logger from './logger';
 import ILogger from './ILogger';
-import { nonHttpTriggerContextWrapper, httpTriggerContextWrapper } from './correlationWrappers';
 import getOperationId from './helpers/getOperationId';
 
-const correlationUtils = {
-  nonHttpTriggerContextWrapper,
-  httpTriggerContextWrapper,
-  getOperationId,
-};
-
-export {
-  Logger, ILogger, correlationUtils,
-};
+export { Logger, ILogger, getOperationId };
+export * from './correlationWrappers';
