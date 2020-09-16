b2e0b6b0f1b8bc0a6ebf3b4e7ed4a2f74a4d84c2
Patrick Blakey <p.blakey@kainos.com>
2020-09-16 10:36:59 +0100

    Remove unused telemetry helpers

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import Logger from './logger';
 import ILogger from './ILogger';
-import Telemetry from './telemetry';
 import azureFunctions from './azureFunctions';
 
 export {
-  Logger, ILogger, Telemetry, azureFunctions,
+  Logger, ILogger, azureFunctions,
 };
