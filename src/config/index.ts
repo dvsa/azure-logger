a11a0472ee3a32d27f5c693559bbd88b57b379fa
Thomas Pritchard <thomaspr@kainos.com>
2021-08-17 13:40:44 +0100

    FTT-11534 remote dotenv

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,13 +1,3 @@
-import dotenv from 'dotenv';
-
-if (!process.env.APPINSIGHTS_INSTRUMENTATIONKEY) {
-  const result = dotenv.config();
-  if (result.error) {
-    // This error should crash whole process
-    throw new Error('⚠️Env vars have not been set and no .env file has been found');
-  }
-}
-
 export default {
 
   logs: {
