f3eb2a62ab4eb63bab6fa36ea9174200c815a99f
Michał Szyszka <michalszy@kainos.com>
2022-05-06 10:09:26 +0200

    Revert "fix linting"
    This reverts commit 9d78b310ca2c1b41e6c62ebd3d438831f89ec226.

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,5 @@ import Logger from './logger';
 import ILogger from './ILogger';
 import getOperationId from './helpers/getOperationId';
 
-export {
-  Logger, ILogger, getOperationId, getCorrelationContext, startOperation, defaultClient,
-};
+export { Logger, ILogger, getOperationId, getCorrelationContext, startOperation, defaultClient };
 export * from './correlationWrappers';
